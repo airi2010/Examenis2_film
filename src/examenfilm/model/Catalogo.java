d6d843bc4e44a90fd5500d4fabac8e91e589fb15
Airam <Airam@DESKTOP-KN2Q17J>
2021-02-02 12:26:34 +0000

    Añadidas toda las funcionalidades básicas

---
+++ src/examenfilm/model/Catalogo.java
@@ -0,0 +1,102 @@
+
+package examenfilm.model;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+
+public class Catalogo {
+    HashMap<Integer,Pelicula> pelimap=new HashMap<>();
+    
+    public Catalogo(HashMap<Integer,Pelicula> pelimap){
+        this.pelimap=pelimap;
+    }
+    
+    public HashMap<Integer,Pelicula> orderbyGenero(){
+        HashMap<Integer,Pelicula> mapgenero=new HashMap<>();
+        ArrayList<String>genero =new ArrayList<String>();
+        for(Pelicula peli: pelimap.values()){
+            if(!genero.contains(peli.getGenero())){
+                genero.add(peli.getGenero());
+            }
+        }
+        Collections.sort(genero); 
+        int temp=0;
+        while(mapgenero.size()!= pelimap.size()){
+            for(Pelicula peli: pelimap.values()){
+                if(peli.getGenero().equals(genero.get(temp))){
+                    mapgenero.put(mapgenero.size(),peli);
+                }
+            }
+            temp++;
+        }
+        
+        return mapgenero;
+    }
+    public HashMap<Integer,Pelicula> orderbyYear(){
+        HashMap<Integer,Pelicula> mapyear=new HashMap<>();
+        ArrayList<Integer>years =new ArrayList<Integer>();
+        for(Pelicula peli: pelimap.values()){
+            if(!years.contains(peli.getyear())){
+                years.add(peli.getyear());
+            }
+        }
+        Collections.sort(years,Collections.reverseOrder()); 
+        int temp=0;
+        while(mapyear.size()!= pelimap.size()){
+            for(Pelicula peli: pelimap.values()){
+                if(peli.getyear()==years.get(temp)){
+                    mapyear.put(mapyear.size(),peli);
+                }
+            }
+            temp++;
+        }
+        
+        return mapyear;
+    }
+    
+    public HashMap<Integer,Pelicula> orderbycalificacion(){
+        HashMap<Integer,Pelicula> mapvalue=new HashMap<>();
+        ArrayList<Integer>values =new ArrayList<Integer>();
+        for(Pelicula peli: pelimap.values()){
+            if(!values.contains((int)peli.getCalificacion())){
+                values.add((int)peli.getCalificacion());
+            }
+        }
+        Collections.sort(values,Collections.reverseOrder()); 
+        int temp=0;
+        while(mapvalue.size()!= pelimap.size()){
+            for(Pelicula peli: pelimap.values()){
+                if((int)peli.getCalificacion()==values.get(temp)){
+                    mapvalue.put(mapvalue.size(),peli);
+                }
+            }
+            temp++;
+        }
+        
+        return mapvalue;
+    }
+    
+    public HashMap<Integer,Pelicula> orderbyviews(){
+        HashMap<Integer,Pelicula> mapviews=new HashMap<>();
+        ArrayList<Integer>views =new ArrayList<Integer>();
+        for(Pelicula peli: pelimap.values()){
+            if(!views.contains(peli.getViews())){
+                views.add(peli.getViews());
+            }
+        }
+        Collections.sort(views,Collections.reverseOrder()); 
+        int temp=0;
+        while(mapviews.size()!= pelimap.size()){
+            for(Pelicula peli: pelimap.values()){
+                if(peli.getViews()==views.get(temp)){
+                    mapviews.put(mapviews.size(),peli);
+                }
+            }
+            temp++;
+        }
+        
+        return mapviews;
+    }
+    
+}
